72bbd7ac3b9b6c6eb46b174eb15a6b07beb00391
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-09-21 17:44:16 -0300

    [tdd][success] Implementando metodo getImpostoDeRenda
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

--- src/app/Cdb.java
+++ src/app/Cdb.java
@@ -5,19 +5,24 @@ public class Cdb {
 	private int quantidadeDias;
 	private double valorAplicado;
 	private double taxaAnual;
-	private double inpostoDeRenda;
+	private double aliquota;
 	
 	public Cdb(int quantidadeDias, double valorAplicado, double taxaAnual, double inpostoDeRenda) {
 		this.quantidadeDias = quantidadeDias;
 		this.valorAplicado = valorAplicado;
 		this.taxaAnual = taxaAnual;
-		this.inpostoDeRenda = inpostoDeRenda;
+		this.aliquota = inpostoDeRenda;
 		
 	}
 	
 	public double getRendimentoBruto() {
 		double tempo = this.quantidadeDias/365.00;
 		double result = (double) Math.round(tempo * this.valorAplicado * (this.taxaAnual/100)*100)/100;
+		return result;
+	}
+
+	public double getImpostoDeRenda() {
+		double result = (double) Math.round(this.getRendimentoBruto() * (this.aliquota/100) *100) /100;
 		return result;
 	}
 }
